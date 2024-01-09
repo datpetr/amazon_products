3e04e03df1134134a1d4dc2225d8ac2aa52c5d2c
master <dat.petr@gmail.com>
2024-01-09 21:10:28 +0300

    fixed categories Book and Books

--- final.py
+++ final.py
@@ -1,167 +1,232 @@
 import pandas as pd
 
-#You might want to change the first pf.read to read a csv file instead of excel. Also the code I wrote does not
-#change the original file.
+# #From now on, for every place we are going to examine the data, we are marking the code as a "note" for efficiency.
+# function for showing the head
+def show_head(df):
+    return df.head()
 
 
+def show_data_type(df):
+    return df.dtypes
+
+
+def show_info(df):
+    return df.info()
+
+# 0. To start with, I have read the file to a dataframe.
 """
-1.	Examine the variables and their data types.
-2.	Examine the head and tail of the data frame. Make sure that you import your data correctly. 
-Check for any separation argument problem (“;” or “,”) of the data, the existence of header in the dataset as well as the existence of NAs.
-3.	Check whether
-a.	Column headers are values, not variable names.
-b.	Multiple variables are stored in one column.
-c.	Variables are stored in both rows and columns.
-d.	Multiple types of observational units are stored in the same table.
-e.	A single observational unit is stored in multiple tables.
- If so, apply data tidying techniques such as stack/unstack, melt, and pivot. 
- Examine the head and tail of the tidy data frame.
-4.	Fix the column names if you detect any typos.
-5.	Drop unnecessary columns.
-6.	Remove the duplicates if it is not the nature of the data. 
-7.	Get rid of any unnecessary strings in the values.
-8.	Remove the white spaces in the string values.
-9.	Be sure that all strings are in the same format (e.g. all in lower-case). If not, correct them.
-10.	Look at the value counts of strings and be sure that all levels of the categories are unique. If not, correct them.
-11.	If you have year, month, and/or day columns, combine them and create a date column.
-12.	Examine the data types again and be sure that numeric variables are float, categorical ones are object,
- and date is in date format. If not, correct it.
-13.	Examine the descriptive statistics of numerical variables. Search for any unusual behavior. 
-Are the variables in the correct range? If not, find the locations and correct them.
-14.	Search for possible outliers. If there are outliers, replace them with the mean.
-15.	Search for uniformity. The units in the numeric columns are in the same format or not. 
-That is, examine whether some data are in meters but some in centimeters. If they are not consistent, 
-convert them into the same units.
-16.	Search for the missing values. Examine their percentage in each column. 
-If the percentage is low, fill them with mean/median/mode. 
-If the percentage is high (e.g >60%-65%), you can drop the column. 
-
+print(show_head(pd.read_excel('data/Amazon.xlsx')))
 """
+# I wanted to skip the title and the empty row in order to have a better view.
+df = pd.read_excel('data/Amazon.xlsx', skiprows=2)
+# let's check the changed data
+"""
+print(show_head(df))
+"""
+df = pd.read_excel('data/Amazon.xlsx', header=None, skiprows=3)
+# after the checking, it seems good. To check the datatype we first name columns
+df.columns = ["Category", "Product ID", "Year", "Quarter", "Quarterly Sales", "Customer Rating", "Seller Rating",
+              "Price", "Discount Rate", "Units Sold", "Revenue", "Shipping Method", "Location", "Advertising Cost"]
+# 1. examine the variables and their datatypes
+"""
+print(show_data_type(df))
+"""
+# there seems to be problem with Discount Rate and Seller Rating, they should ve integer instead of floats.
+# We are going to do this change later since it will be much easier
+# 2. remove any separation error in variables.
 
+# since we defined the header there is no need to check the existence of it
+# check the exising of NA's
+"""
+print(df.isnull().any())
+"""
+# it is showed that there are NA's values in Seller Rating, Advertising Cost, Quarterly Sales
 
-#0 -- To start with, I have read the file to a dataframe. I wanted to skip the title and the empty row in order to have a better view.
-df = pd.read_excel('Amazon.xlsx')
-#print(df.head)
-#I checked how it is looking and it seemed fine to me.
+# Check if the column contains different types of values
+for column in df.columns:
+    unique_types = df[column].apply(type).unique()
 
-#1 - EXAMINE THE VARIABLES AND THEIR DATA TYPES
+    # Display the result
+    if len(unique_types) > 1:
+        print(f"The column: {column} contains different types: {unique_types}")
+    else:
+        print(f"The column: {column} contains only one type.")
+# so, there is a problem only in The column: Quarterly Sales, we will consider it later
 
-#2 - Examine the head and tail of the data frame. Make sure that you import your data correctly.
-# Check for any separation argument problem (“;” or “,”) of the data,
-# the existence of header in the dataset as well as the existence of NAs.
-#print(df.head)
-#It seems that there are 3 rows in the beginning and some NAs
-df = pd.read_excel('Amazon.xlsx', header = None, skiprows = 3)
+# Remove specified characters from string columns
+df.replace(to_replace=[r'\{', r'\}', r';', r':', r',', r'\t', r'\n'], value='', regex=True, inplace=True)
 
-#YOU MIGHT WANT TO CHANGE THE CODE HERE BECAUSE I DON'T THINK THAT IT DELETES THE ROWS IN THE ACTUAL FILE
+# remove the all whitespaces
+df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 
-#I leave the question 3 on the above to you
+# 4 we already fixed the column names
 
-#4 -- Then I wanted to begin the process with fixing the column names.
-df.columns = ["Category","Product ID","Year","Quarter","Quarterly Sale","Customer Rating","Seller Rating","Price",
-                 "Discount Rate","Units Sold","Revenue","Shipping Method","Location","Advertising Cost"]
+# 5 - The product ID column is unnecessary for data visualization, so I'm deleting it. Ask why to Liza? I have no idea what she is thinling about a
+# I don't understand women
+# df = df.drop("Product ID", axis=1)
 
-#5 - The product ID column is unnecessary for data visualization, so I'm deleting it.
-df = df.drop("Product ID", axis=1)
-
-#6 - Now I'm going to eliminate the duplicates since they are not in the nature of the data.
+# 6 - Now I'm going to eliminate the duplicates since they are not in the nature of the data.
 duplicates = df.duplicated()
 df.drop_duplicates(inplace=True)
-#And by this, dropped the duplicates.
+# And by this, dropped the duplicates.
+# 7 - Now I will check if we have any unnecessary strings in the values.
+# we already checked it
 
-#7 - Now I will check if we have any unnecessary strings in the values.
-print(df.dtypes)
-#It seems that there are no unnecessary strings in numerical values
+# 8 - Now to remove the white spaces before the strings
+# we already did it
 
-#8 - Now to remove the white spaces before the strings
-df["Shipping Method"] = df["Shipping Method"].str.lstrip()
-#And it's done.
+# 9 - To format the string values we will check the names for nominal values
+# I will do it later
 
-#9 - To format the string values we will check the names for nominal values
-#You could write a code that creates a list of names in every categorical column (and years and quarters)(and customer/seller rating),
-# like the list could be equal to something like [[Beauty, BEAUTY, Book, Books,...],[EU,US,European Union..] etc.]
-# then you can use the codes below to change che names accordingly.
-# You can use "value.counts()" function from pandas btw
-"""
-#I write the needed code to format the names in the category column and fix the inconsistency.
+# make all titles in same format
 df["Category"] = df["Category"].str.title()
-df["Shipping Method"].replace({"Book": "Books", "Home - Kitchen": "Home & Kitchen"}, inplace=True)
---------------------------------------------------------------------
-The below is for fixing years column 
 
-df["YEAR"] = df["YEAR"].astype(str)
-df["YEAR"] = df["YEAR"].str[:4]
-df["YEAR"] = pd.to_numeric(df["YEAR"], errors='coerce')
+# 10 - We don't have to check if levels of categories are unique since we don't need it to be unique for this data set.
 
-The below is for fixing customer ratings
+# 11 - We don't need to create a date column since we only have the quarter and year
 
-df["CustomerRating"] = abs(df["Customer Rating"])
----------------------------------------------------------------
-The below is for fixing the format in location column
+# 12 - since we only have the year and quarter as the date's values,
+# we should change "Year" (Quarter is already "object") to object
+df['Year'] = df['Year'].astype(str)
 
-wrong_notation_list = []
-for row in df["Location"]:
-    if row not in ["EU", "US", "ASIA"]:
-        if row not in wrong_notation_list:
-            wrong_notation_list.append(row)
-# print(wrong_notation_list) I checked the list of wrong notations.
-df["Shipping Method"].replace({"European Union": "EU", "United States": "US", "Asia": "ASIA"}, inplace=True)
-------------------------------------------------------------------
-"""
+# I will fix the datatypes later since if I do it now it will collapse the code
 
-#10 - We don't have to check if levels of categories are unique since we don't need it to be unique for this data set.
+# 13.	Examine the descriptive statistics of numerical variables. Search for any unusual behavior.
+# Are the variables in the correct range? If not, find the locations and correct them.
+# first of all let's find NA values in all columns
 
-#11 - We don't need to create a date column since we only have the quarter and year
+for col in df.columns:
+    na_amount = df[col].isna().sum()
+    if na_amount != 0:
+        print(f'{col} has {na_amount} NA values')
 
-#12 - It says that we should make the date values's types as "date" but I don't know how to do it.
+# we've found that values in Quarterly Sales is Revenue divide on Units Sold. So, instead of replacing
+# all data by the mean/median/mode we can just replace it by Revenue divide on Units Sold
+df['Quarterly Sales'] = df['Quarterly Sales'].replace('NA', pd.NA)
+df['Quarterly Sales'] = df['Quarterly Sales'].fillna(df["Revenue"] // df["Units Sold"])
+df['Quarterly Sales'] = df['Quarterly Sales'].astype(int)
+# In the seller rating there are NA values. So, let's replace them by
+# median of rating since all data in integer we should use meidna
+df['Seller Rating'].fillna(df['Seller Rating'].median(), inplace=True)
 
-#Im leaving 13, 14 and 15 to you. For 15 you only need to consider the discount rate since it has "%" sign on some of
-#the values. You can handle it with the code below
+# In the Advertisment cost it is impossible to estimate a advertismant cost to the product and we have only two NA
+# we wouldn't lose so much if we remove two string and also all our data is integer we should change the datatype
+# to integer
 
-"""
-for index, value in enumerate(df["Discount Rate"]):
-    #if type(value) != int:
-        #print(value)by this, I saw the problem is that since some of the values were written with "%" symbol, 
-        some of the rates were interpreted as small floats
-    continue
-    
-mask = df["Discount Rate"] < 1
-df.loc[mask, "Discount Rate"] = df.loc[mask, "Discount Rate"] * 100
-df["Discount Rate"] = df["Discount Rate"].astype(str).str.replace('%', '')
-df["Discount Rate"] = pd.to_numeric(df["Discount Rate"], errors='coerce')
-df["Discount Rate"] = df["Discount Rate"].astype(int)
--------------------------------------------------------------------------
+# df = df.dropna(subset=['Advertising Cost'])
+# df["Advertising Cost"] = df["Advertising Cost"].astype(int)
+# df['Advertising Cost'].fillna(df['Advertising Cost'].mean(), inplace=True)
+df['Advertising Cost'].fillna(df['Advertising Cost'].mean(), inplace=True)
 
-"""
+# range of rating is unlikely to be negative since almost all data is ranging
+# from 0 to 5. we should change the negative values to positive one
+# also convert all data to integer since all data is integer numbers
+df["Customer Rating"] = df["Customer Rating"].abs().astype(int)
 
-#16 - At last I checked the rows with missing values.
-'''for column in df:
-    null_count = df[column].isnull().sum()
-    percentage_null = (null_count / len(df)) * 100
-    print(f"{column} has {percentage_null:.2f}% null values")
-'''
-#By this I checked the null percentages, and it checks all the values are 98%,
-# so I wanted to continue with deleting empty rows. (I haven't used dropna since the rows were written as "NA", not NaN
-problematic_values = df[df['Quarterly Sale'].str.strip() == 'NA']
-df = df[df['Quarterly Sales'].str.strip() != 'NA']
-df['Quarterly Sales'] = pd.to_numeric(df['Quarterly Sales'])
+# some data in year is typed in a wring for example instead of 2019 there is written 20199
 
-"""
+df['Year'] = df['Year'].str[:4]
+
+
+
+# the discount rate has a different values one values with "%" other not let's convert all values to one datatype
+df['Discount Rate'] = pd.to_numeric(df['Discount Rate'], errors='coerce')
+df['Discount Rate'] = df["Discount Rate"].astype(int)
+
+# 14 outliers
+# first, we need to write a code which will check whether in column exist outliers or not
+# Function to identify outliers using IQR
+def identify_outliers_by_category(df, category_column, numerical_column):
+    categories = df[category_column].unique()
+    outliers_dict = {}
+
+    for category in categories:
+        subset = df[df[category_column] == category]
+        column = subset[numerical_column]
+
+        Q1 = column.quantile(0.25)
+        Q3 = column.quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 3 * IQR
+        upper_bound = Q3 + 3 * IQR
+        outliers = column[(column < lower_bound) | (column > upper_bound)]
+
+        # Check if the number of outliers doesn't exceed 10% of the category's data
+        if not outliers.empty:
+            category_count = len(subset)
+            outlier_count = len(outliers)
+            if outlier_count / category_count <= 0.1:
+                outliers_dict[category] = outliers
+
+    return outliers_dict
+
+outliers_by_category = {}
+for numerical_column in df.select_dtypes(include='number').columns:
+    if numerical_column not in ["Customer Rating", "Seller Rating", "Discount Rate"]:
+        outliers_by_category[numerical_column] = {}
+        for category_column in ['Category']:
+            outliers_by_category[numerical_column][category_column] = identify_outliers_by_category(df, category_column,
+                                                                                                    numerical_column)
+# Displaying outliers for each category
+for numerical_column, category_dict in outliers_by_category.items():
+    print(f"\nOutliers in {numerical_column} by Category:")
+    for category_column, outliers in category_dict.items():
+        if outliers:
+            for cat, out_vals in outliers.items():
+                print(f"Outliers in {cat}: {out_vals.values}")
+        else:
+            print(f"No outliers in {category_column}")
+
+# Replacing outliers by mean of the specific columns within each category
+for numerical_column, category_dict in outliers_by_category.items():
+    for category_column, outliers in category_dict.items():
+        for cat, out_vals in outliers.items():
+            category_mean = df.loc[df[category_column] == cat, numerical_column].mean().astype(int)
+            df.loc[(df[category_column] == cat) & df[numerical_column].isin(out_vals), numerical_column] = category_mean
+
+
+
+# def replace_outliers_with_mean(column):
+#     Q1 = column.quantile(0.25)
+#     Q3 = column.quantile(0.75)
+#     IQR = Q3 - Q1
+#     lower_bound = Q1 - 1.5 * IQR
+#     upper_bound = Q3 + 1.5 * IQR
+#
+#     # Identify and replace outliers
+#     outliers = column[(column < lower_bound) | (column > upper_bound)]
+#     df.loc[outliers.index, column.name] = column.mean()
+#
+#     return column
+#
+#
+# # Replace outliers in each numeric column
+# for column in df.select_dtypes(include=['number']).columns:
+#
+#     df[column] = replace_outliers_with_mean(df[column])
+
+
+# make all data in columns in one type
+df['Category'] = df['Category'].replace({'BEAUTY': 'Beauty',
+                                         'ELECTRONICS': 'Electronics',
+                                         'Home - Kitchen': 'Home & Kitchen',
+                                         'Book': 'Books'})
+df['Location'] = df['Location'].replace({'United States': 'US', 'ASIA': 'Asia', 'European Union': 'EU'})
+
+# change all strings to lower case
+
+# change the datatypes of variables where it needed
+df['Quarterly Sales'] = df['Quarterly Sales'].astype(float)
+df['Seller Rating'] = df['Seller Rating'].astype(float)
+df['Advertising Cost'] = df['Advertising Cost'].astype(float)
+# change all variables to lower case
+# df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+
 
 #And with this step, the data cleaning part is done.
+# Save data to Excel file and csv file
+csv_filename = 'data/filtered_amazon.csv'
+df.to_csv(csv_filename, index=False)
 
-
-
-"""
-WE WILL USE THE PART 3 ONLY IF WE CHOOSE TO NOT DROP THE PRODUCT ID COLUMN
-#3 -- Then I wanted to continue with changing the product id code for books. I thought that changing it to BK would fix the confusion
-modified_id = []
-category = df["Category"]
-id = df["Product ID"]
-
-for i, cat_value in enumerate(category):
-    if cat_value == "Books":
-        id[i] = 'BK' + id[i][1:]
-df["Product ID"] = id
-#print(df["Product ID"][27:])
-"""
+csv_filename = 'data/filtered_amazon.xlsx'
+df.to_excel(csv_filename, index=False)
